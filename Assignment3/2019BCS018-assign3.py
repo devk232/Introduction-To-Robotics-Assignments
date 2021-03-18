3f18e766466e25324a7270be36a6552441e39756
devk232 <devsadana999@gmail.com>
2021-03-18 17:30:35 +0530

    added 3rd assignment

---
+++ Assignment3/2019BCS018-assign3.py
@@ -0,0 +1,29 @@
+import math
+import numpy as np 
+
+a, b =  list(map(int, input("enter dimensions of DH matrix - ").strip().split()))
+dh_marrix = np.zeros((a,b))
+
+print("Enter DH matrix - ")
+for i in range(a) :
+  dh_marrix[i] = list(map(int, input().strip().split()))
+
+#this function returns transformation matrix T_ij
+def transform(i , j):
+   theta_i = np.deg2rad(dh_marrix[i][3])
+   link_length_j = dh_marrix[j][1]
+   twist_j = np.deg2rad(dh_marrix[j][0])
+   link_offset_i = dh_marrix[i][2]
+   return np.array([[np.cos(theta_i), np.sin(theta_i), 0, link_length_j],
+                    [np.cos(twist_j) * np.sin(theta_i), np.cos(twist_j) *
+                     np.cos(theta_i), np.sin(twist_j), -link_offset_i * np.sin(twist_j)],
+                    [np.sin(twist_j) * np.sin(theta_i), np.sin(twist_j) *
+                     np.cos(theta_i), np.cos(twist_j), link_offset_i * np.cos(twist_j)],
+                    [0, 0, 0, 1]])
+
+matrix = np.identity((4));
+
+for i in range(1, 7):
+  matrix = matrix@(transform(i, i - 1))
+
+print(matrix)
